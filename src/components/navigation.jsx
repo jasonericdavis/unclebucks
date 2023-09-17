55358c31b4207a81cb358fad09ffd67b2ef5c10e
Jason Davis <14043845+jasonericdavis@users.noreply.github.com>
2023-09-17 19:40:24 -0400

    wip

---
+++ src/components/navigation.jsx
@@ -0,0 +1,19 @@
+const NavItem = ({title}) => {
+  return  (
+    <li>
+      <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{title}</a>
+    </li>
+  )
+}
+
+export const NavItems = () => {
+  return (
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <NavItem title={`Home`} />
+        <NavItem title={`Who Are We`} />
+        <NavItem title={`What We Do`} />
+      </ul>
+    </div>
+  )
+}
