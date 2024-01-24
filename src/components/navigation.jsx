ef1f797139363ff829b8b66e8d46772464987bd6
Jason Davis <14043845+jasonericdavis@users.noreply.github.com>
2024-01-23 21:19:23 -0500

    wip

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,7 +1,18 @@
 const NavItem = ({title}) => {
   return  (
     <li>
-      <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{title}</a>
+      {/* <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{title}</a> */}
+      <a href="#" className="
+        block 
+        py-2 pl-3 pr-4 
+        text-white-300 
+        rounded hover:bg-gray-100 
+        md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 
+        dark:text-white md:dark:hover:text-blue-500 
+        dark:hover:bg-gray-700 
+        dark:hover:text-white 
+        md:dark:hover:bg-transparent"
+      >{title}</a>
     </li>
   )
 }
